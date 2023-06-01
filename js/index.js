eb66277366537d1fffbcae2c6f1cf9fa3cf70fa6
Mirah Moon <130616222+MirahMoon@users.noreply.github.com>
2023-06-01 01:50:31 +0000

    Wed, May 31, 2023, 6:50 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 const contacts = document.getElementById('contacts')
 
+var a = document.createElement('a');
+      var linkText = document.createTextNode("Contacts/Socials");
+      a.appendChild(linkText);
+      a.title = "Contacts/Socials";
+      a.href = "contacts.html";
+      document.body.appendChild(a);
+
 contacts.addEventListener('mouseover', () => {
     contacts.innerHTML = "Contacts/Socials"
 })
 
 contacts.addEventListener('mouseout', () => {
-    contacts.innerHTML = ""
+    contacts.innerHTML = a
 })
