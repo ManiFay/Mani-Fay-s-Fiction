d21d0b7a7d6688ec353c43ae3bab4c850d6d6c2e
Mirah Moon <130616222+MirahMoon@users.noreply.github.com>
2023-06-03 21:58:01 +0000

    Sat, Jun 3, 2023, 2:58 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const credit = document.getElementById('credit')
 
 credit.addEventListener('mouseover', () => {
-    credit.innerHTML = "Credit for this art goes to my brother."
+    credit.innerHTML = "Credit for this art goes to my brother. I, Mani Fay, wrote the article."
 })
 
 credit.addEventListener('mouseout', () => {
-    credit.innerHTML = "Hover over this text to see the credits for the artist!"
+    credit.innerHTML = "Hover over this text to see the credits for the artist and article!"
 })
